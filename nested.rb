2900befcd7934fa780c7ca8d273c69995cce7a8f
Chris Wade <swade781977@yahoo.com>
2020-02-10 23:15:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+  return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+ return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,14 +57,12 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+   return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+ 
 
 	programmer_hash = 
  		{
@@ -78,9 +78,10 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
+        
      }
 
-    
+  programmer_hash[:yukihiro_matsumoto] = [:known_for "Ruby", :languages ["LISP", "C"]
 end
 
 def changing_alan
@@ -102,7 +103,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+  programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
